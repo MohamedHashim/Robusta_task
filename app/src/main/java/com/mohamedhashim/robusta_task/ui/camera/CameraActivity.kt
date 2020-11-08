e465a99a98e14d0f352c3152fba3b53d255b390d
MohamedHashim <sawmido2000@gmail.com>
2020-11-08 22:18:21 +0200

    Implement customized bitmap banner with text

--- app/src/main/java/com/mohamedhashim/robusta_task/ui/camera/CameraActivity.kt
+++ app/src/main/java/com/mohamedhashim/robusta_task/ui/camera/CameraActivity.kt
@@ -5,7 +5,6 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
-import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
